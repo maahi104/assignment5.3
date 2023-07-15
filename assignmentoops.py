2e2c2c916ba55ffab01e227a16d3203377b55950
vaishali tiwari <136571044+maahi104@users.noreply.github.com>
2023-07-15 21:54:04 +0530

    Update assignmentoops.py

--- assignmentoops.py
+++ assignmentoops.py
@@ -22,12 +22,15 @@ class SavingsAccount(Account):
         return self.balance * (self.interest_rate / 100)
 
 
-account = Account("Ashish", 5000)
-account.deposit(1000)
-print(account.get_balance())  
-account.withdrawal(500)
-print(account.get_balance())  
+title = input("Enter account title: ")
+balance = int(input("Enter account balance: "))
+interest_rate = int(input("Enter interest rate: "))
 
-savings_account = SavingsAccount("Ashish", 5000, 5)
+account = Account(title, balance)
+account.deposit(5000)
+print(account.get_balance())  
+ 
+
+savings_account = SavingsAccount(title, balance, interest_rate)
 interest_amount = savings_account.interest_amount()
-print(interest_amount)  
+print(interest_amount)
